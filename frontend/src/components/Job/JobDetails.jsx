dadaf9103050dd1132c097e76e1ca44dd2b6dbed
Vidushi Singhal <singhalvidushi7417529406@gmail.com>
2024-03-17 01:29:11 +0530

    add URL
    URL

--- frontend/src/components/Job/JobDetails.jsx
+++ frontend/src/components/Job/JobDetails.jsx
@@ -12,7 +12,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/v1/job/${id}`, {
+      .get(`https://jobportal-y6p3.onrender.com/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
